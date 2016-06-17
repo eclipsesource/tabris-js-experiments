0a8935bc3c8f29e048c17a09581e2c614aee10b6
Shai Alon <shaialon84@gmail.com>
2016-06-17 17:58:04 +0300

    Nice animation for deleting a post

--- tabris-js-backendless/src/components/post.js
+++ tabris-js-backendless/src/components/post.js
@@ -1,5 +1,5 @@
-import {Page, TabFolder, Composite, Tab, ui,TextView, ImageView} from 'tabris';
-import {FULL} from './../styles/layouts';
+import {Page, TabFolder, Composite, Tab, ui,TextView, ImageView, ActivityIndicator} from 'tabris';
+import {FULL, HIDE, SHOW ,CENTER, INVISIBLE , VISIBLE} from './../styles/layouts';
 import {getIconSrc} from './../styles/icons';
 import {BACKGROUND, BORDER, WHITE} from './../styles/colors';
 import Avatar from './avatar';
@@ -50,20 +50,24 @@ export default class extends Composite {
 	super(postLayout.container);
 	this.updateElements = this.updateElements.bind(this); //ES6 hack
 	this.itemOptions = this.itemOptions.bind(this);
+	this.deletePost = this.deletePost.bind(this);
 
 	let _elements = {};
 	this.append(
 	  new Composite(postLayout.border).append(
 		new Composite(postLayout.canvas).append(
+		  _elements.postContent = new Composite(FULL).append(
+			// Actual Content goes here !
+			_elements.avatar = new Avatar(null,{top: 10, height: 40, left: 10, width: 40}),
+			_elements.creator = new TextView({top: 10, height: 40, left: 60, right: 10}),
+			new Composite({top: 10, right: 10, height: 40, width: 40, highlightOnTouch:true}).append (
+			  _elements.options = new ImageView({top: 5, right: 5, height: 20, width: 20 , image: getIconSrc('menu_small')})
+			).on('tap', this.itemOptions),
+			_elements.title = new TextView({bottom: 10, height: 40, left: 10, right: 10}),
+			_elements.image = new ImageView({...FULL, top:60, bottom: 60})
+		  ),
 
-		  // Actual Content goes here !
-		  _elements.avatar = new Avatar(null,{top: 10, height: 40, left: 10, width: 40}),
-		  _elements.creator = new TextView({top: 10, height: 40, left: 60, right: 10}),
-		  new Composite({top: 10, right: 10, height: 40, width: 40, highlightOnTouch:true}).append (
-			_elements.options = new ImageView({top: 5, right: 5, height: 20, width: 20 , image: getIconSrc('menu_small')})
-		  ).on('tap', this.itemOptions),
-		  _elements.title = new TextView({bottom: 10, height: 40, left: 10, right: 10}),
-		  _elements.image = new ImageView({...FULL, top:60, bottom: 60})
+		  _elements.loading = new ActivityIndicator({...CENTER, ...INVISIBLE})
 
 		)
 	  )
@@ -73,9 +77,10 @@ export default class extends Composite {
 
   updateElements(item){
 	let _elements = this.get('_elements');
+	_elements.postContent.set(SHOW);
 	_elements.creator.set({text: item.creator ? item.creator.name : 'Anonymous'});
 	_elements.title.set({text:item.title});
-
+	_elements.loading.set(INVISIBLE);
 	updateImage(_elements.image,item.image);
 	_elements.avatar.setEmail(item.creator ? item.creator.email : null);
 
@@ -99,23 +104,30 @@ export default class extends Composite {
 	  console.log("SELECTED: "+SharingOptions[buttonIndexChosen].name);
 	  SharingOptions[buttonIndexChosen].handler(_activeItem);
 
-	}, deleteCB => {
+	}, this.deletePost);
+  }
 
-	  deletePost(_activeItem)
-		.then(res => {
-		  console.log("DELETED THIS POST");
-		  this.trigger('deleted',_activeItem);
-		})
-		.catch(err => {
-		  console.log("FAILED DELETING THIS POST");
-		  console.error(err);
-		});
-
+  deletePost() {
+	let _activeItem = this.get('_activeItem');
+	// Animate delete
+	let _elements = this.get('_elements');
+	_elements.loading.set(VISIBLE);
+	_elements.postContent.animate(HIDE, {
+	  duration: 200,
+	  easing: "ease-out",
 	});
 
 
-
-
+	// Request delete
+	deletePost(_activeItem)
+	  .then(res => {
+		console.log("DELETED THIS POST");
+		this.trigger('deleted',_activeItem);
+	  })
+	  .catch(err => {
+		console.log("FAILED DELETING THIS POST");
+		console.error(err);
+	  });
   }
 
 
