b4be0848ba245033b3a71d0b13226e86e2593911
Shai Alon <shaialon84@gmail.com>
2016-06-17 17:01:43 +0300

    documentation

--- tabris-js-backendless/src/services/BackendLess.js
+++ tabris-js-backendless/src/services/BackendLess.js
@@ -7,6 +7,11 @@
  *  3. File management - Upload images taken on the device. Scale on Server (TODO).
  *  4. Realtime communication between clients via Pubsub
  *  5. Push notifications? (TODO)
+ *
+ *
+ *  Note: while this app conveys some security rules - for instance only you can delete your posts (not other users),
+ *  You also have to enforce security permissions in Backendless
+ *
  */
 
 /*******************
